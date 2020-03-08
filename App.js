e92bb7a7c3cbae9069b83579bfc8fd767dc7df4a
chuanshuoge <zchen2019@zoho.com>
2020-03-08 15:12:09 -0600

    2nd

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import * as Font from 'expo-font'
 import * as Location from 'expo-location'
 import * as TaskManager from 'expo-task-manager'
 import useInterval from 'react-useinterval'
+import openMap from 'react-native-open-maps'
 
 const LOCATION_TASK_NAME = 'background-location-task';
 let background_location = null
@@ -28,6 +29,7 @@ export default function App() {
   const [speed, setspeed] = useState(-1)
   const [time, settime] = useState('')
   const [updatePeriod, setupdatePeriod] = useState(null)
+  const [address, setaddress] = useState(null)
 
   useEffect(() => {
     initializeApp()
@@ -75,6 +77,52 @@ export default function App() {
     }
   }
 
+  AccordionHeader = (item, expanded) => {
+    return (
+      <View style={{
+        flexDirection: "row",
+        padding: 10,
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#A9DAD6"
+      }}>
+        <Text style={{ fontWeight: "600" }}>
+          {" "}{item.title}
+        </Text>
+        {expanded
+          ? <Icon style={{ fontSize: 18 }} name="remove-circle" />
+          : <Icon style={{ fontSize: 18 }} name="add-circle" />}
+      </View>
+    );
+  }
+  AccordionContent = (item) => {
+    return (
+      <Text
+        style={{
+          backgroundColor: "#A9DAD6",
+          padding: 10,
+          fontStyle: "italic",
+        }}
+      >
+        {item.content}
+      </Text>
+    );
+  }
+
+  search_address = async () => {
+    if (background_location) {
+      const _address = await Location.reverseGeocodeAsync(background_location.coords)
+      setaddress(_address[0])
+    }
+  }
+
+  open_map = () => {
+    if (background_location) {
+      const e = background_location.coords
+      openMap({ latitude: e.latitude, longitude: e.longitude })
+    }
+  }
+
   if (permission_status !== 'granted') {
     <Container style={{ backgroundColor: '#1C2833' }}>
       <Text>App needs location permission granted</Text>
@@ -88,20 +136,40 @@ export default function App() {
   return (
     <Container style={{ backgroundColor: '#1C2833' }}>
       <Content style={{ marginTop: 25 }}>
-        <Text style={{ color: 'white' }}>accuracy: {acc}</Text>
-        <Text style={{ color: 'white' }}>altitude: {altitude}</Text>
-        <Text style={{ color: 'white' }}>heading: {heading}</Text>
-        <Text style={{ color: 'white' }}>latitude: {latitude}</Text>
-        <Text style={{ color: 'white' }}>longitude: {longitude}</Text>
-        <Text style={{ color: 'white' }}>speed: {speed}</Text>
-        <Text style={{ color: 'white' }}>Time: {time}</Text>
+        <Accordion dataArray={[{
+          title: 'Realtime GPS',
+          content: 'Accuracy: ' + acc + ' Meters\n' +
+            'Altitude: ' + altitude + ' Meters\n' +
+            'Heading: ' + heading + ' Degree\n' +
+            'Latitude: ' + latitude + ' Degree\n' +
+            'Longitude: ' + longitude + ' Degree\n' +
+            'Speed: ' + speed + ' M/S\n' +
+            'Time: ' + time
+        }, {
+          title: 'Address (update location first)',
+          content: address ?
+            address.name + ' ' + address.street + ' ' + address.city + '\n' +
+            address.region + ' ' + address.country + ' ' + address.postalCode
+            : 'Waiting for GPS location'
+        }]}
+          expanded={0}
+          renderHeader={AccordionHeader}
+          renderContent={AccordionContent} />
 
-        <View style={{ flexDirection: 'row', justifyContent: "space-around" }}>
-          <Button style={{ width: 150, justifyContent: "center" }} onPress={() => setupdatePeriod(1000)}>
+        <View style={{ flexDirection: 'row', justifyContent: "space-around", marginTop: 10 }}>
+          <Button small style={{ width: 150, justifyContent: "center" }} onPress={() => setupdatePeriod(1000)}>
             <Text>Update Location</Text>
           </Button>
-          <Button style={{ width: 150, justifyContent: "center" }} onPress={() => stop_update_location()}>
+          <Button small style={{ width: 150, justifyContent: "center" }} onPress={() => stop_update_location()}>
             <Text>Stop Update</Text>
+          </Button>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: "space-around", marginTop: 10 }}>
+          <Button small style={{ width: 150, justifyContent: "center" }} onPress={() => search_address()}>
+            <Text>Search Address</Text>
+          </Button>
+          <Button small style={{ width: 150, justifyContent: "center" }} onPress={() => open_map()}>
+            <Text>Open Map</Text>
           </Button>
         </View>
       </Content>
